acfe268ae90c755132b17e0db29fcd9e65d54cb9
Shunya0308 <tagushun11@icloud.com>
2021-08-27 16:08:36 +0900

    all git push

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgrespl_psycopg2',
         'NAME': 'db.sqlite3',
         'USER': 'user',
         'PASSWORD': '',
